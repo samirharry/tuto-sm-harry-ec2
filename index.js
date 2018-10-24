1181734230d6d4dc2824953bfae413de555490ca
Samir Harry Huamán Patricio <samirharry13@gmail.com>
2018-10-24 11:44:21 -0500

    Html added

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express= require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World Hi How are you I miss you');
-})
+app.use(express.static('public'));
+
 app.listen(3000,()=>{
     console.log('Corriendo Puerto 3000');
 })
